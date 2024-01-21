a1d71351fd0d26d5603e32ce60bf9720ee184526
hjdevelop <hyunjun6133@naver.com>
2024-01-22 07:39:27 +0900

    [Fix] : 일자별 임시 데이터 클래스 추가

--- app/src/main/java/com/example/weather_app/data/model/DailyDataModel.kt
+++ app/src/main/java/com/example/weather_app/data/model/DailyDataModel.kt
@@ -1,10 +1,18 @@
 package com.example.weather_app.data.model
 
-//데이터 구조 생각하기
-data class DailyDataModel (
-    val day : String,
-    val date : String,
-    val weather : String,
-    val maxTemp : Int,
-    val minTemp : Int
+data class DailyDataModel(
+    val day: String,        //요일
+    val date: String,       //날짜
+    val weather: String,    //날씨
+    val maxTemp: String,    //최고 온도
+    val minTemp: String     //최저 온도
+)
+
+data class TempDaily(
+    val fcstDate: String,
+    val fcstTime: String,
+    val maxTemp: String,
+    val minTemp: String,
+    val sky: String,
+    val rainType: String
 )
