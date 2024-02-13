773bfdaea6ffa2d10e6759bd72c1dd55c844e63a
hjdevelop <hyunjun6133@naver.com>
2024-02-13 22:09:09 +0900

    [Fix] : API 데이터 로딩 후 텍스트 보이도록 수정

--- app/src/main/java/com/example/weather_app/ui/bookmark/BookmarkListAdapter.kt
+++ app/src/main/java/com/example/weather_app/ui/bookmark/BookmarkListAdapter.kt
@@ -3,6 +3,7 @@ package com.example.weather_app.ui.bookmark
 import android.annotation.SuppressLint
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
@@ -75,6 +76,10 @@ class BookmarkListAdapter :
         @SuppressLint("SetTextI18n")
         fun bind(bookmarkItem: BookmarkDataModel) {
             with(binding) {
+                bookmarkTvTemp.visibility = View.INVISIBLE
+                bookmarkTvMaxTemp.visibility = View.INVISIBLE
+                bookmarkTvMinTemp.visibility = View.INVISIBLE
+
                 if (bookmarkItem.Dong.isEmpty()) {
                     bookmarkTvLocation.text = bookmarkItem.Gu
                 } else {
@@ -123,8 +128,13 @@ class BookmarkListAdapter :
                     Log.d("itemInfo", "${minTemp}, ${maxTemp}, ${temp}")
 
                     withContext(Dispatchers.Main) {
-                        bookmarkTvMaxAndMin.text = "최고 ${maxTemp}° 최저 ${minTemp}°"
+                        bookmarkTvMinTemp.text = "${minTemp}°"
+                        bookmarkTvMaxTemp.text = "${maxTemp}°"
                         bookmarkTvTemp.text = "${temp}°"
+
+                        bookmarkTvTemp.visibility = View.VISIBLE
+                        bookmarkTvMaxTemp.visibility = View.VISIBLE
+                        bookmarkTvMinTemp.visibility = View.VISIBLE
                     }
                 }
 
