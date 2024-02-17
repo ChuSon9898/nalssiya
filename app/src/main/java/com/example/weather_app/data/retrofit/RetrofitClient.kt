de448a74259b74120cee61edf0ec79cc516452a7
hjdevelop <hyunjun6133@naver.com>
2024-02-17 21:43:43 +0900

    [Fix] : SocketTimeOut 오류 수정

--- app/src/main/java/com/example/weather_app/data/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/weather_app/data/retrofit/RetrofitClient.kt
@@ -6,6 +6,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     val client = Retrofit
@@ -17,6 +18,9 @@ object RetrofitClient {
                 .addInterceptor(HttpLoggingInterceptor().apply {
                     level = HttpLoggingInterceptor.Level.BODY
                 })
+                .connectTimeout(100, TimeUnit.SECONDS)
+                .readTimeout(100, TimeUnit.SECONDS)
+                .writeTimeout(100, TimeUnit.SECONDS)
                 .build()
         )
         .build()
