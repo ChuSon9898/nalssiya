663080a9e98cd009bba6e7985f5f712800a5116b
hjdevelop <hyunjun6133@naver.com>
2024-07-07 17:44:41 +0900

    [Refactor] : RepositoryModule에 DataBaseModule 추가

--- app/src/main/java/com/example/weather_app/di/RepositoryModule.kt
+++ app/src/main/java/com/example/weather_app/di/RepositoryModule.kt
@@ -3,9 +3,11 @@ package com.example.weather_app.di
 import com.example.weather_app.data.repository.retrofit.DailyTempRepositoryImpl
 import com.example.weather_app.data.repository.retrofit.DailyWeatherRepositoryImpl
 import com.example.weather_app.data.repository.retrofit.HourlyRepositoryImpl
+import com.example.weather_app.data.repository.room.BookmarkRepositoryImpl
 import com.example.weather_app.domain.repository.retrofit.DailyTempRepository
 import com.example.weather_app.domain.repository.retrofit.DailyWeatherRepository
 import com.example.weather_app.domain.repository.retrofit.HourlyRepository
+import com.example.weather_app.domain.repository.room.BookmarkRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -26,4 +28,8 @@ abstract class RepositoryModule {
     @Singleton
     @Binds
     abstract fun hourlyRepository(hourlyRepositoryImpl: HourlyRepositoryImpl) : HourlyRepository
+
+    @Singleton
+    @Binds
+    abstract fun bookmarkRepository(bookmarkRepositoryImpl: BookmarkRepositoryImpl) : BookmarkRepository
 }
